7b00e31641102fd98e53711de8093c4bd4efadc2
Felix Yu <flyyufelix@gmail.com>
2017-04-14 09:14:17 +0800

    Add InceptionV4

---
+++ inception_v4.py
@@ -0,0 +1,307 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+import copy
+
+from keras.layers.convolutional import MaxPooling2D, Convolution2D, AveragePooling2D
+from keras.layers import Input, merge, Dropout, Dense, Flatten, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.optimizers import SGD
+from keras import backend as K
+from keras.utils.layer_utils import convert_all_kernels_in_model
+from keras.utils.data_utils import get_file
+
+
+#########################################################################################
+# Implements the Inception Network v4 (http://arxiv.org/pdf/1602.07261v1.pdf) in Keras. #
+#########################################################################################
+
+#TH_WEIGHTS_PATH = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.0/inception-v4_weights_th_dim_ordering_th_kernels.h5'
+#TF_WEIGHTS_PATH = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.0/inception-v4_weights_tf_dim_ordering_tf_kernels.h5'
+
+def conv2d_bn(x, nb_filter, nb_row, nb_col,
+              border_mode='same', subsample=(1, 1), bias=False):
+    """
+    Utility function to apply conv + BN. 
+    (Slightly modified from https://github.com/fchollet/keras/blob/master/keras/applications/inception_v3.py)
+    """
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+    x = Convolution2D(nb_filter, nb_row, nb_col,
+                      subsample=subsample,
+                      border_mode=border_mode,
+                      bias=bias)(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = Activation('relu')(x)
+    return x
+
+def block_inception_a(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 96, 1, 1)
+
+    branch_1 = conv2d_bn(input, 64, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 96, 3, 3)
+
+    branch_2 = conv2d_bn(input, 64, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
+    branch_2 = conv2d_bn(branch_2, 96, 3, 3)
+
+    branch_3 = AveragePooling2D((3,3), strides=(1,1), border_mode='same')(input)
+    branch_3 = conv2d_bn(branch_3, 96, 1, 1)
+
+    x = merge([branch_0, branch_1, branch_2, branch_3], mode='concat', concat_axis=channel_axis)
+    return x
+
+
+def block_reduction_a(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 384, 3, 3, subsample=(2,2), border_mode='valid')
+
+    branch_1 = conv2d_bn(input, 192, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 224, 3, 3)
+    branch_1 = conv2d_bn(branch_1, 256, 3, 3, subsample=(2,2), border_mode='valid')
+
+    branch_2 = MaxPooling2D((3,3), strides=(2,2), border_mode='valid')(input)
+
+    x = merge([branch_0, branch_1, branch_2], mode='concat', concat_axis=channel_axis)
+    return x
+
+
+def block_inception_b(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 384, 1, 1)
+
+    branch_1 = conv2d_bn(input, 192, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 224, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 256, 7, 1)
+
+    branch_2 = conv2d_bn(input, 192, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 192, 7, 1)
+    branch_2 = conv2d_bn(branch_2, 224, 1, 7)
+    branch_2 = conv2d_bn(branch_2, 224, 7, 1)
+    branch_2 = conv2d_bn(branch_2, 256, 1, 7)
+
+    branch_3 = AveragePooling2D((3,3), strides=(1,1), border_mode='same')(input)
+    branch_3 = conv2d_bn(branch_3, 128, 1, 1)
+
+    x = merge([branch_0, branch_1, branch_2, branch_3], mode='concat', concat_axis=channel_axis)
+    return x
+
+
+def block_reduction_b(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 192, 1, 1)
+    branch_0 = conv2d_bn(branch_0, 192, 3, 3, subsample=(2, 2), border_mode='valid')
+
+    branch_1 = conv2d_bn(input, 256, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 256, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 320, 7, 1)
+    branch_1 = conv2d_bn(branch_1, 320, 3, 3, subsample=(2,2), border_mode='valid')
+
+    branch_2 = MaxPooling2D((3, 3), strides=(2, 2), border_mode='valid')(input)
+
+    x = merge([branch_0, branch_1, branch_2], mode='concat', concat_axis=channel_axis)
+    return x
+
+
+def block_inception_c(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    branch_0 = conv2d_bn(input, 256, 1, 1)
+
+    branch_1 = conv2d_bn(input, 384, 1, 1)
+    branch_10 = conv2d_bn(branch_1, 256, 1, 3)
+    branch_11 = conv2d_bn(branch_1, 256, 3, 1)
+    branch_1 = merge([branch_10, branch_11], mode='concat', concat_axis=channel_axis)
+
+
+    branch_2 = conv2d_bn(input, 384, 1, 1)
+    branch_2 = conv2d_bn(branch_2, 448, 3, 1)
+    branch_2 = conv2d_bn(branch_2, 512, 1, 3)
+    branch_20 = conv2d_bn(branch_2, 256, 1, 3)
+    branch_21 = conv2d_bn(branch_2, 256, 3, 1)
+    branch_2 = merge([branch_20, branch_21], mode='concat', concat_axis=channel_axis)
+
+    branch_3 = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
+    branch_3 = conv2d_bn(branch_3, 256, 1, 1)
+
+    x = merge([branch_0, branch_1, branch_2, branch_3], mode='concat', concat_axis=channel_axis)
+    return x
+
+
+def inception_v4_base(input):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    # Input Shape is 299 x 299 x 3 (th) or 3 x 299 x 299 (th)
+    net = conv2d_bn(input, 32, 3, 3, subsample=(2,2), border_mode='valid')
+    net = conv2d_bn(net, 32, 3, 3, border_mode='valid')
+    net = conv2d_bn(net, 64, 3, 3)
+
+    branch_0 = MaxPooling2D((3,3), strides=(2,2), border_mode='valid')(net)
+
+    branch_1 = conv2d_bn(net, 96, 3, 3, subsample=(2,2), border_mode='valid')
+
+    net = merge([branch_0, branch_1], mode='concat', concat_axis=channel_axis)
+
+    branch_0 = conv2d_bn(net, 64, 1, 1)
+    branch_0 = conv2d_bn(branch_0, 96, 3, 3, border_mode='valid')
+
+    branch_1 = conv2d_bn(net, 64, 1, 1)
+    branch_1 = conv2d_bn(branch_1, 64, 1, 7)
+    branch_1 = conv2d_bn(branch_1, 64, 7, 1)
+    branch_1 = conv2d_bn(branch_1, 96, 3, 3, border_mode='valid')
+
+    net = merge([branch_0, branch_1], mode='concat', concat_axis=channel_axis)
+
+    branch_0 = conv2d_bn(net, 192, 3, 3, subsample=(2,2), border_mode='valid')
+    branch_1 = MaxPooling2D((3,3), strides=(2,2), border_mode='valid')(net)
+
+    net = merge([branch_0, branch_1], mode='concat', concat_axis=channel_axis)
+
+    # 35 x 35 x 384
+    # 4 x Inception-A blocks
+    for idx in xrange(4):
+      net = block_inception_a(net)
+
+    # 35 x 35 x 384
+    # Reduction-A block
+    net = block_reduction_a(net)
+
+    # 17 x 17 x 1024
+    # 7 x Inception-B blocks
+    for idx in xrange(7):
+      net = block_inception_b(net)
+
+    # 17 x 17 x 1024
+    # Reduction-B block
+    net = block_reduction_b(net)
+
+    # 8 x 8 x 1536
+    # 3 x Inception-C blocks
+    for idx in xrange(3):
+      net = block_inception_c(net)
+
+    return net
+
+
+def inception_v4_model(img_rows, img_cols, color_type=1, num_classes=None, dropout_keep_prob=0.2):
+    '''
+    Inception V4 Model for Keras
+
+    Model Schema is based on
+    https://github.com/kentsommer/keras-inceptionV4
+
+    ImageNet Pretrained Weights 
+    Theano: https://github.com/kentsommer/keras-inceptionV4/releases/download/2.0/inception-v4_weights_th_dim_ordering_th_kernels.h5
+    TensorFlow: https://github.com/kentsommer/keras-inceptionV4/releases/download/2.0/inception-v4_weights_tf_dim_ordering_tf_kernels.h5
+
+    Parameters:
+      img_rows, img_cols - resolution of inputs
+      channel - 1 for grayscale, 3 for color 
+      num_class - number of class labels for our classification task
+    '''
+
+    # Input Shape is 299 x 299 x 3 (tf) or 3 x 299 x 299 (th)
+    if K.image_dim_ordering() == 'th':
+        inputs = Input((3, 299, 299))
+    else:
+        inputs = Input((299, 299, 3))
+
+    # Make inception base
+    net = inception_v4_base(inputs)
+
+
+    # Final pooling and prediction
+
+    # 8 x 8 x 1536
+    net_old = AveragePooling2D((8,8), border_mode='valid')(net)
+
+    # 1 x 1 x 1536
+    net_old = Dropout(dropout_keep_prob)(net_old)
+    net_old = Flatten()(net_old)
+
+    # 1536
+    predictions = Dense(output_dim=1001, activation='softmax')(net_old)
+
+    model = Model(inputs, predictions, name='inception_v4')
+
+    if K.image_dim_ordering() == 'th':
+      # Use pre-trained weights for Theano backend
+      weights_path = 'cache/inception-v4_weights_th_dim_ordering_th_kernels.h5'
+    else:
+      # Use pre-trained weights for Tensorflow backend
+      weights_path = 'cache/inception-v4_weights_tf_dim_ordering_tf_kernels.h5'
+
+    model.load_weights(weights_path, by_name=True)
+
+    # Truncate and replace softmax layer for transfer learning
+    # Cannot use model.layers.pop() since model is not of Sequential() type
+    # The method below works since pre-trained weights are stored in layers but not in the model
+    net_ft = AveragePooling2D((8,8), border_mode='valid')(net)
+    net_ft = Dropout(dropout_keep_prob)(net_ft)
+    net_ft = Flatten()(net_ft)
+    predictions_ft = Dense(output_dim=num_class, activation='softmax')(net_ft)
+
+    model = Model(inputs, predictions_ft, name='inception_v4')
+
+    # Learning rate is changed to 0.001
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+if __name__ == '__main__':
+
+    # Fine-tune Example
+    img_rows, img_cols = 299, 299 # Resolution of inputs
+    channel = 3
+    num_class = 10 
+    batch_size = 16 
+    nb_epoch = 3
+
+    # TODO: Load training and validation sets
+    X_train, X_valid, Y_train, Y_valid = load_data()
+
+    # Load our model
+    model = inception_v4_model(img_rows, img_cols, channel, num_class, dropout_keep_prob=0.2)
+
+    # Start Fine-tuning
+    model.fit(train_data, test_data,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              shuffle=True,
+              verbose=1,
+              validation_data=(X_valid, Y_valid),
+              )
+
+    # Make predictions
+    predictions_valid = model.predict(X_valid, batch_size=batch_size, verbose=1)
+
+    # Cross-entropy loss score
+    score = log_loss(Y_valid, predictions_valid)
